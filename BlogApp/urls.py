fe02a9e6c78e6e64858b449cfb964ef42d501958
Asadbek16 <asadbekshomurodov96@gmail.com>
2023-02-02 16:02:52 +0500

    Men harbir postni titleni link qildim. Shu title bosganda post haqidagi malumotlarni barchasini ko'rsatadi.

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import All_Post_Show_View
+from .views import ( 
+                        All_Post_Show_View, 
+                        See_Only_One_Post_View
+                    )
 
 
 urlpatterns = [
-    path('', All_Post_Show_View.as_view(), name='all_post_page')
+    path('', All_Post_Show_View.as_view(), name='all_post_page'),
+    path('post/<int:pk>/', See_Only_One_Post_View.as_view(), name='only_post')
 ]
--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 class All_Post_Show_View(ListView):
     model = Post
     template_name = 'all_post_view.html'
+
+
+class See_Only_One_Post_View(DetailView):
+    model = Post
+    template_name = 'see_only_one_post.html'
+
