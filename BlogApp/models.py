bb4b3ed02480bbb89d9dd2f35e7e9c9287788187
Asadbek16 <asadbekshomurodov96@gmail.com>
2023-02-02 15:22:35 +0500

    Post nomli model yaratildi unga title, author, text, short_info nomli column_name lar berildi, Va Post nomli model admin paneliga qo'shildi

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    short_info = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
